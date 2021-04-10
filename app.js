263f4c7ff4386e9ad2d6c4dde9d0fdb119ef2533
lusifer2644A <64474004+lusifer2644A@users.noreply.github.com>
2021-04-10 14:51:10 +0530

    1.0

--- app.js
+++ app.js
@@ -8,3 +8,66 @@ for(var i=0;i<wip.length;i++) {
     console.log("Check");
     wip[i].addEventListener('click',alertwip,false);
 }
+
+
+
+
+// Jquery for button scrolls
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        // e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+
+// For skills animation
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+tl.from(".scroll-anim-span ",0.5,{width:0});
+
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".skill-activate",
+            triggerHook: "onLeave",
+            duration: "50%",
+})
+  .setPin(".skills-anim-span")
+  .setTween(tl)
+		.addTo(controller);
+
+
+
+function updatePercentage() {
+  //percent.innerHTML = (tl.progress() *100 ).toFixed();
+  tl.progress();
+  console.log(tl.progress());
+}
+
+
+
+
+//  Nav Bar
+
+const hamburger=document.querySelector(".hamburger");
+const nav_ul=document.querySelector("#navbar ul");
+const links=document.querySelector("#navbar ul li");
+
+hamburger.addEventListener('click',(e)=>{
+    nav_ul.classList.toggle('open');
+})
+
+links.forEach(link => {
+    link.classList.toggle('fade');
+});
